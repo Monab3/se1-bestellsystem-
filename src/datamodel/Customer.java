4d9aeb42802960e18952c2e56f87dc31dfdf5225
Mona <monabecher@gmx.de>
2020-12-16 18:50:57 +0100

    tests

--- src/datamodel/Customer.java
+++ src/datamodel/Customer.java
@@ -17,32 +17,29 @@ public String getId() {
 	return id;
 }
 public String getFirstName() {
+	if (firstName == null) 
+		firstName ="";
 	return firstName;
 }
 public void setFirstName(String firstName) {
 	this.firstName = firstName;
 }
 public String getLastName() {
+	if (lastName == null) lastName = "";
 	return lastName;
 }
 public void setLastName(String lastName) {
+
 	this.lastName = lastName;
+	
 }
 
 public String getContact() {
+	if(contact == null) contact ="";
 	return contact;
 }
 public void setContact(String contact) {
 	this.contact=contact;
 }
-
-
-
-
-
-
-
-
-
 
 }
